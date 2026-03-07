398a61236fc4051699c7887bb8416ca68a21fe07
Ahmed Salah <Ahmedsalahali@hotmail.com>
2026-03-07 17:40:22 +0200

    Initial Commit

---
+++ TaskFlow/TaskFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowApp.swift
+//  TaskFlow
+//
+//  Created by Ahmed Salah on 07/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
